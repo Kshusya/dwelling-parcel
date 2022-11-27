5b7f3bf3a1fa8407f66a073ddf3a6b12b0ce589f
Ksenia Peresedova <kshusya.yay@gmail.com>
2022-11-27 23:06:30 +0300

    change reviews carousel code

--- src/reviews-carousel.js
+++ src/reviews-carousel.js
@@ -2,32 +2,31 @@ const slides = document.getElementsByClassName("review");
 const dots = document.getElementsByClassName("reviews-block__dot");
 
 let slidePosition = 0;
-const totalSlides = slides.length;
 
-function updateSlides() {
-    slides[slidePosition].classList.add("review_visible");
-    dots[slidePosition].classList.add("reviews-block__dot_active");
-}
-
-function onButtonClick(event) {
-    const clickedButton = event.currentTarget;
-    
+function updateReview(review_id) {
     for (let i = 0; i < slides.length; i++) {
         const slide = slides[i];
         const dot = dots[i];
 
         slide.classList.remove("review_visible");
         dot.classList.remove("reviews-block__dot_active");
-
-        if (dot === clickedButton)
-            slidePosition = i;
     }
 
-    clickedButton.classList.add("reviews-block__dot_active");
-    updateSlides();
+    slidePosition = review_id;
+    
+    slides[slidePosition].classList.add("review_visible");
+    dots[slidePosition].classList.add("reviews-block__dot_active");
+}
+
+function onButtonClick(event) {
+    updateReview(Array.prototype.indexOf.call(dots, event.currentTarget));
 }
 
 for (let dot of dots) {
-        dot.addEventListener("click", onButtonClick);
-    }
+    dot.addEventListener("click", onButtonClick);
+}
+
+setInterval(() => {
+    updateReview((slidePosition + 1) % dots.length);
+}, 5000)
 
